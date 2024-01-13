d88e7b738b92887b0c92e70866e4635b239fcf4f
khazarmammadov <132160383+khazarmammadov@users.noreply.github.com>
2024-01-13 19:22:18 +0400

    Add files via upload

---
+++ Employee-Management-System-in-CSharp-main/EmployeeManagementSystem/EmployeeManagementSystem/SqlScripts/employees.sql
@@ -0,0 +1,15 @@
+﻿create table employees
+ (  
+    id int primary key identity(1,1),
+    employee_id varchar(max) null,
+    full_name varchar(max) null,
+    gender varchar(max) null,
+    contact_number varchar(max) null,
+    position varchar(max) null,
+    image varchar(max) null,
+    salary int null,
+    insert_date varchar null,
+    update_date varchar null,
+    delete_date varchar null,
+    status varchar(max) null
+ );
---
+++ Employee-Management-System-in-CSharp-main/EmployeeManagementSystem/EmployeeManagementSystem/SqlScripts/users.sql
@@ -0,0 +1,8 @@
+﻿ create table users 
+ (
+    id int primary key identity(1,1),
+    username varchar(max) null,
+    password varchar(max) null,
+    date_register date null
+ );
+
